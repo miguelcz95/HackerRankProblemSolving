61a11d3a2ed4cd4ead937ed6cd9694b75ec91145
Miguel Campuzano <miguelcz95>
2021-05-03 08:15:55 -0500

    added the solution to main day of the programmer problem

---
+++ src/com/company/MainDayOfTheProgrammer.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class MainDayOfTheProgrammer {
+
+    //INFO: https://www.hackerrank.com/challenges/day-of-the-programmer/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+    public static void main(String[] args) {
+        
+    }
+}
